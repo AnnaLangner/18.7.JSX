35e01bc1c073f2cf571fa932a64d5798db7c30e6
Ania <annalangner06@gmail.com>
2019-05-22 15:04:13 +0200

    Using the JSX syntax in the Contact.js file

--- components/Contact.js
+++ components/Contact.js
@@ -4,17 +4,16 @@ var Contact = React.createClass({
 	},
 	render: function () {
 		return (
-			React.createElement('div', {className: 'contactItem'},
-				React.createElement('img', {
-					className: 'contactImage',
-					src: './image/user-circle.png'
-				}),
-				React.createElement('p', {className: 'contactLabel'}, 'Name: ' + this.props.item.firstName),
-                React.createElement('p', {className: 'contactLabel'}, 'Last Name: ' + this.props.item.lastName),
-                React.createElement('a', {className: 'contactEmail',  href: 'mailto:' + this.props.item.email},
-                    this.props.item.email
-                )
-			)
+			<div className={'contactItem'}>
+				<img className={'contactImage'} src={'./image/user-circle.png'} />
+				<p className={'contactLabel'}>
+					Name: {this.props.item.firstName},
+					Last Name: {this.props.item.lastName}
+				</p>
+				<a href={'mailto:' + this.props.item.email}>
+					{this.props.item.email}
+				</a>
+			</div>
 		)
-	},
+	}
 });
